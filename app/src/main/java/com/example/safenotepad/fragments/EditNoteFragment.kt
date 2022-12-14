b6832ebce06fed98c57737fc36038d52b8b20509
koziej97 <lukkoz2@st.amu.edu.pl>
2022-12-14 21:28:14 +0100

    added delete note button with confirmation dialog

--- app/src/main/java/com/example/safenotepad/fragments/EditNoteFragment.kt
+++ app/src/main/java/com/example/safenotepad/fragments/EditNoteFragment.kt
@@ -1,14 +1,20 @@
 package com.example.safenotepad.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
 import com.example.safenotepad.databinding.FragmentEditNoteBinding
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.MutableLiveData
+import com.example.safenotepad.R
 import com.example.safenotepad.data.EncryptedSharedPreferencesDataStorage
 import com.example.safenotepad.SharedViewModel
 import javax.crypto.Cipher
@@ -38,6 +44,10 @@ class EditNoteFragment : Fragment() {
         binding.saveEditButton.setOnClickListener {
             saveNote()
         }
+
+        binding.deleteButton.setOnClickListener {
+            createConfirmAlertForDeleteButton(requireContext())
+        }
     }
 
     override fun onDestroyView() {
@@ -61,6 +71,20 @@ class EditNoteFragment : Fragment() {
         val encryptedSharedPreferences = EncryptedSharedPreferencesDataStorage(requireContext())
         encryptedSharedPreferences.saveNote(encryptedNote)
         encryptedSharedPreferences.saveIv(cipher.iv)
+    }
+
+    private fun createConfirmAlertForDeleteButton(context: Context){
+        AlertDialog.Builder(requireActivity())
+            .setTitle(context.resources.getString(R.string.confirm_delete_button))
+            .setPositiveButton("Yes") { _, _ ->
+                Toast.makeText(context, "Deleting note...", Toast.LENGTH_LONG).show()
+                findNavController().navigate(
+                    EditNoteFragmentDirections.actionEditNoteFragmentToNotesFragment())
+            }
+            .setNegativeButton("No", null)
+            .setCancelable(false)
+            .create()
+            .show()
     }
 
 }
